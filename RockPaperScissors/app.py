351378b070cc6e081d7053076191e4164c2b618e
Adrian Gallant <adrian.gallant@student.fairfield.edu>
2022-02-22 11:27:29 -0500

    updated todos

--- RockPaperScissors/app.py
+++ RockPaperScissors/app.py
@@ -2,7 +2,7 @@
 # I did my own version, then made improvements inspired from (notably dictionary, enums, exceptions)
 # https://realpython.com/python-rock-paper-scissors/#play-several-games-in-a-row https://cocalc.com
 # https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3#introduction
-from flask import Flask, render_template
+from flask import Flask
 from game.game import game_blueprint
 from authentication.authentication import authentication_blueprint
 
@@ -12,6 +12,8 @@ app.register_blueprint(game_blueprint, url_prefix='/game')
 
 if __name__ == "__main__":
     app.run(debug=True)
-# TODO: ADD DATABASE FOR CPU/PLAYER STAT TRACKING?
-# TODO: ADD FRONTEND
+
 # TODO: ADD DISTINCT USER FUNCTIONALITY
+# TODO: LOCAL MULTIPLAYER
+# TODO: ONLINE MULTIPLAYER
+
